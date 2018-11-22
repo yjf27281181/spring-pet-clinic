a7beef14192613a11662d8c1b8ad4d63ec13f165
long may the sunshine <31409032+yjf27281181@users.noreply.github.com>
2018-11-21 22:28:19 -0800

    creating interfaces

---
+++ pet-clinic-data/src/main/java/csu/springframework/petclinic/services/PetService.java
@@ -0,0 +1,14 @@
+package csu.springframework.petclinic.services;
+
+import csu.springframework.petclinic.model.Pet;
+
+import java.util.Set;
+
+public interface PetService {
+
+    Pet findById(Long id);
+
+    Pet save(Pet pet);
+
+    Set<Pet> findAll();
+}
---
+++ pet-clinic-data/src/main/java/csu/springframework/petclinic/services/VetService.java
@@ -0,0 +1,14 @@
+package csu.springframework.petclinic.services;
+
+import csu.springframework.petclinic.model.Vet;
+
+import java.util.Set;
+
+public interface VetService {
+
+    Vet findById(Long id);
+
+    Vet save(Vet vet);
+
+    Set<Vet> findAll();
+}
