b03eeae8e81bfa7d3572b731e565788e28689b18
long may the sunshine <31409032+yjf27281181@users.noreply.github.com>
2018-11-21 22:09:33 -0800

    refactoring to multimodule maven project.

--- pet-clinic-web/src/test/java/springpetclinic/SpringPetClinicApplicationTests.java
+++ pet-clinic-web/src/test/java/springpetclinic/SpringPetClinicApplicationTests.java
@@ -1,12 +1,13 @@
 package springpetclinic;
 
+import csu.springframework.petclinic.SpringPetClinicApplication;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.context.junit4.SpringRunner;
 
 @RunWith(SpringRunner.class)
-@SpringBootTest
+@SpringBootTest(classes = SpringPetClinicApplication.class)
 public class SpringPetClinicApplicationTests {
 
     @Test
