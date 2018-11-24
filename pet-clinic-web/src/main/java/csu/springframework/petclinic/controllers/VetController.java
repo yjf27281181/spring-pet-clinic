6429465b6bf06c2254fe103a1cd296f72b7d85f6
long may the sunshine <31409032+yjf27281181@users.noreply.github.com>
2018-11-24 13:48:10 -0800

    adding static files

--- pet-clinic-web/src/main/java/csu/springframework/petclinic/controllers/VetController.java
+++ pet-clinic-web/src/main/java/csu/springframework/petclinic/controllers/VetController.java
@@ -1,13 +1,22 @@
 package csu.springframework.petclinic.controllers;
 
+import csu.springframework.petclinic.services.VetService;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 @Controller
 public class VetController {
+    private final VetService vetService;
+
+    public VetController(VetService vetService) {
+        this.vetService = vetService;
+    }
 
     @RequestMapping({"/vets", "/vets/index", "/vets/index.html"})
-    public String ListVets() {
+    public String ListVets(Model model) {
+        System.out.println(vetService.findAll());
+        model.addAttribute("vets", vetService.findAll());
         return "vets/index";
     }
 }
