18f7256aaa7469f75e1814cac2cd337d58286b5d
long may the sunshine <31409032+yjf27281181@users.noreply.github.com>
2018-11-27 23:13:45 -0800

    Implemented base class(JPA). closes #11. closes #12.

--- pet-clinic-data/src/main/java/csu/springframework/petclinic/model/Vet.java
+++ pet-clinic-data/src/main/java/csu/springframework/petclinic/model/Vet.java
@@ -1,9 +1,16 @@
 package csu.springframework.petclinic.model;
 
+import javax.persistence.*;
 import java.util.HashSet;
 import java.util.Set;
 
+@Entity
+@Table(name="vets")
 public class Vet extends Person {
+    @ManyToMany(fetch = FetchType.EAGER)
+    @JoinTable(name = "vet_specialities",
+            joinColumns = @JoinColumn(name="vet+id"),
+            inverseJoinColumns = @JoinColumn(name="speciality_id"))
     private Set<Speciality> specialities = new HashSet<>();
 
     public Set<Speciality> getSpecialities() {
